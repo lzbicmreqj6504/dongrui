dfd8306e07aa80e9874d3a7eaa4f1dc0730a16f9
17600037756@163.com <17600037756@163.com>
2023-06-03 16:08:08 +0800

    tabbar

---
+++ pages/djrw/djrw.js
@@ -0,0 +1,111 @@
+// pages/djrw/djrw.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        kssj:'',
+        jssj:'',
+        activityArr: [
+            { id: 1, label: '型号1' },
+            { id: 2, label: '型号2' },
+            { id: 3, label: '型号3' },
+            { id: 4, label: '型号4' }
+          ],
+        setIndex:'1',
+        switch1Checked: true,
+        box_list:[
+            {
+                name:'DCDC',
+                bjlx:'通用故障',
+                gzsj:'2023-05-24 14:06',
+                jcsj:'2023-05-24 14:06',
+                cxsj:'50分钟',
+                type:'bcblue'
+            },{
+                name:'DCDC',
+                bjlx:'通用故障',
+                gzsj:'2023-05-24 14:06',
+                jcsj:'2023-05-24 14:06',
+                cxsj:'50分钟',
+                type:'bcred'
+            },{
+                name:'DCDC',
+                bjlx:'通用故障',
+                gzsj:'2023-05-24 14:06',
+                jcsj:'2023-05-24 14:06',
+                cxsj:'50分钟',
+                type:'bcblue'
+            },{
+                name:'DCDC',
+                bjlx:'通用故障',
+                gzsj:'2023-05-24 14:06',
+                jcsj:'2023-05-24 14:06',
+                cxsj:'50分钟',
+                type:'bcred'
+            },
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+    ksdj(){
+        wx.navigateTo({
+          url: '../fqdjrw/fqdjrw',
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
--- pages/fktRw/fktRw.js
+++ pages/fktRw/fktRw.js
@@ -5,7 +5,42 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        background: [],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 2000,
+        duration: 500,
+        elements: [{
+            title: '点检任务',
+            name: 'Dianjian',
+            nav:'djrw',
+            color: 'cyan',
+            icon: 'edit'
+          },
+          {
+            title: '保养任务',
+            name: 'Baoyang',
+            color: 'blue',
+            icon: 'newshot',
+            nav:'byrw',
+          },
+          {
+            title: '维修任务',
+            name: 'Weixiu',
+            color: 'purple',
+            icon: 'settingsfill',
+            nav:'wxrw',
+          },
+          {
+            title: '生产任务 ',
+            name: 'Shengchan',
+            color: 'mauve',
+            icon: 'expressman',
+            nav:'scrw',
+          },
+          
+        ],
     },
 
     /**
@@ -14,12 +49,38 @@ Page({
     onLoad(options) {
 
     },
+    getImg(){
+        let that = this
+        wx.request({
+            url:'https://owl.zjzenbo.com/api/banner', //必填，其他的都可以不填
+            
+            header:{  
+               'content-type':'application/json'
+            },
+            method:'GET',  
+            dataType:'JSON',  
+            responseType:'text', 
+            success(res){
+                console.log(res);
+                let data  = JSON.parse(res.data)
+                that.setData({
+                    background:data
+                })
+            },
+            fail(){  
+                console.log('fail')
+            },
+            complete(){   
+                 console.log('complete')   
+            }
+        }  )  
+    },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady() {
-
+        this.getImg()
     },
 
     /**
